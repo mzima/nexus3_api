121b429a2d221a1c23da0b6550d9f129ad0d8a1c
mzima <martin.zimmermann@gmx.com>
2019-08-20 15:03:23 +0200

    Feature: Added test for docker group repositories

--- tests/all.sh
+++ tests/all.sh
@@ -30,6 +30,7 @@ function upload_api(){
 function create(){
     http_send 'repo_docker_hosted' '{"action":"create","port":12345,"id":"00_test_docker"}'
     http_send 'repo_raw_hosted' '{"action":"create","id":"00_test_raw"}'
+    http_send 'repo_docker_group' '{"action":"create","port":54321,"id":"00_test_docker_group","members":["00_test_docker"]}'
     http_send 'content_selector' '{"action":"create","id":"00_test","expression":"format==\"docker\""}'
     http_send 'privilege' '{"action":"create","id":"00_test","properties":{"contentSelector":"00_test","repository":"00_test_docker","actions":"browse,read,edit"}}'
     http_send 'role' '{"action":"create","id":"00_test","privileges":["00_test"],"roles":["00_test"]}'
@@ -41,6 +42,7 @@ function delete(){
     http_send 'role' '{"action":"delete","id":"00_test"}'
     http_send 'privilege' '{"action":"delete","id":"00_test"}'
     http_send 'content_selector' '{"action":"delete","id":"00_test"}'
+    http_send 'repo_docker_group' '{"action":"delete","id":"00_test_docker_group"}'
     http_send 'repo_docker_hosted' '{"action":"delete","id":"00_test_docker"}'
     http_send 'repo_raw_hosted' '{"action":"delete","id":"00_test_raw"}'
 }
